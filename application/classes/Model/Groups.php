356a132dab55bf44a5cd60da8f36405c43be6285
mokung <1546217296@qq.com>
2016-10-28 15:48:22 +0800

    新建组功能

---
+++ application/classes/Model/Groups.php
@@ -0,0 +1,46 @@
+<?php  defined('SYSPATH') OR die('No direct access allowed.');
+
+class Model_Groups extends Model_Base {
+
+	static $table = 'groups';
+    static $primary_key = 'group_id';
+
+    static $cols = array(
+        'group_id',
+        'time_created',
+        'defunct',
+    );
+
+
+    public $group_id;
+    public $time_created;
+    public $defunct;
+    
+  public function save()
+    {
+        // prepare data
+        //        $this->data['update_at'] = PP::format_time();
+
+        // 过滤不存在的数据
+        $data = $this->raw_array();
+
+        // else save new record
+        $keys   = array_keys($data);
+        $values = array_values($data);
+
+        $query  = DB::insert(static::$table, $keys)->values($values);
+
+        $query->execute();
+    }
+
+
+    protected function initial_data()
+    {
+      // $this->group_id = "dw";
+       $this->defunct = self::DEFUNCT_NO;
+       $this->time_created = e::format_time();
+    }
+
+       public function validate()
+    {}
+}
