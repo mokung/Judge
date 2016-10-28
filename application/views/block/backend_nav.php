4acaf0719e200261898871b9b6b625a0641bcc63
zhang ze <605166577@qq.com>
2016-10-28 16:26:37 +0800

    solve conflic

--- application/views/block/backend_nav.php
+++ application/views/block/backend_nav.php
@@ -5,6 +5,12 @@
     <li <?php if (Request::$current->controller() == 'Contest'):?>class="active" <?php endif;?>><a href="<?php e::url('admin/contest');?>"><?php echo(__('top_backend.contest')); ?></a></li>
     <li <?php if (Request::$current->controller() == 'News'):?>class="active" <?php endif;?>><a href="<?php e::url('admin/news');?>"><?php echo(__('top_backend.news')); ?></a></li>
     <li <?php if (Request::$current->controller() == 'Setting'):?>class="active" <?php endif;?>><a href="<?php e::url('admin/setting');?>"><?php echo(__('top_backend.configure')); ?></a></li>
+
+
+
+
+
+
     <li <?php if (Request::$current->controller() == 'Invite'):?>class="active" <?php endif;?>><a href="<?php e::url('/admin/Invite/index');?>"><?php echo(__("invitation_code")); ?></a></li>
 
 </ul>
