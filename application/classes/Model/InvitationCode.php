8959428df0df30b8d7b6f74357781aa4f475028a
mokung <1546217296@qq.com>
2016-11-03 09:46:18 +0800

    Update InvitationCode.php

--- application/classes/Model/InvitationCode.php
+++ application/classes/Model/InvitationCode.php
@@ -31,18 +31,14 @@ class Model_InvitationCode extends Model_Base
     public $createtime;
 
 
+
     const MAIL_NEW  = 1;
     const MAIL_READ = 0;
-
-
-
 /*
 anthod: zhang ze xiang
 function : generate invitation code
 date: 2016.10.31 13:02
  */
-
-
 public static function generateRandomString($length = 6) {
     $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
     $charactersLength = strlen($characters);
@@ -53,26 +49,65 @@ public static function generateRandomString($length = 6) {
     return $randomString;
 }
 
+    public static function get_Group_id($invitation)
+    {
+       
+        $filter = array(
+            'invited_code' => $invitation,
+        );
 
+        $result = self::find($filter);
 
+        if ($result) return $result[0];
 
+        return null;
+    }
+    public static function invitation_del($invited_code){
 
+         $query = DB::delete(self::$table)
+            ->where('invited_code', '=', $invited_code);
+        return $query->execute();
 
+    }
+ 
+      public function save()
+    {
+        // prepare data
+//        $this->data['update_at'] = PP::format_time();
 
+        // 过滤不存在的数据
+        $data = $this->raw_array();
 
+        if ( isset($this->{static::$primary_key}) and $this->{static::$primary_key})
+        {
+            // if primary key exist, then update, contain primary key, haha
+            $primary_id = $this->{static::$primary_key};
+            //            unset($this->data[static::$primary_key]);
 
+            $query = DB::update(static::$table)->set($data)->where(static::$primary_key, '=', $primary_id);
+            $ret   = $query->execute();
 
+            return $ret;
+        } else
+        {
+            // else save new record
+            $keys   = array_keys($data);
+            $values = array_values($data);
 
+            list($id, $affect_row) = DB::insert(static::$table, $keys)->values($values)->execute();
 
+            $this->{static::$primary_key} = $id;
 
-   // not to change
-
+            return $affect_row;
+        }
+    }
+   
     protected function initial_data()
     {
-        $this->new_mail = self::MAIL_NEW;
-        $this->in_date = e::format_time();
-        $this->reply = 0;
-        $this->defunct = self::DEFUNCT_NO;
+       $this->new_mail = self::MAIL_NEW;
+       $this->in_date = e::format_time();
+       $this->reply = 0;
+       $this->defunct = self::DEFUNCT_NO;
     }
 
     public function validate()
