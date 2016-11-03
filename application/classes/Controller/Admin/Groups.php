2f1590a3c69f233ffc813210490aaf236fe0a927
mokung <1546217296@qq.com>
2016-11-03 09:39:22 +0800

    Update Groups.php

--- application/classes/Controller/Admin/Groups.php
+++ application/classes/Controller/Admin/Groups.php
@@ -10,21 +10,19 @@ class Controller_Admin_Groups extends Controller_Admin_Base{
 	public function action_create(){
 
 			$group_id  = $_GET['id'];
-			$group =  new Model_Groups;
-			$group->group_id = $group_id;
-			$group->save();
+      $group = Model_Groups::find_by_id($group_id);
+
+      if (!$group) {
+           $group =  new Model_Groups;
+          $group->group_id = $group_id;
+          $group->save();
 
 
-			    // if ( $group->save() )
-       //       {
-     		// $this->template_data['title'] = $group_id;
-       //$this->redirect('/admin/groups/index');
-            // } else {
-            	$this->template_data['title'] = $group['group_id'];
-              // $this->flash_error(__('common.error'));
-           // }
-		// $this->template_data['id'] =$group_id;
-
+      }else{
+        $this->flash_error(array(__('common.group_exist')));
+      }
+			
+      $this->redirect('admin/groups/');
 
 	}
 
@@ -41,42 +39,5 @@ class Controller_Admin_Groups extends Controller_Admin_Base{
         $this->action_index();
     }
 
-  public function action_status()
-  {
-    # code...
-
-        $this->view = 'admin/situation/status';
-
-                 $title = __('ddd');
-        $this->template_data['title'] = $title;
-
-
-        $group_id = Arr::get($_GET,'id');
-        $date = Arr::get($_GET,'date');
-
-        $this->template_data['group_id'] = $group_id;
-        $this->template_data['date'] = $date;
-
-        $order_by = array(
-                'date' => Model_Base::ORDER_ASC
-            );
-
-        $result = Model_Situation::search($date,'date',$order_by,$show_all=true, 'group_id', $group_id);
-
-         $this->template_data['oneday'] = $result;
-
-
-        //get per status count
-        foreach ($result as $line) {
-          # code...
-
-        }
-
-
-
-
-
-  }
-
-
+	
 }
