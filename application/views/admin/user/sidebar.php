2169931f4a0813b8ca6e9adf521fa7c6087e252e
zl <854976866@qq.com>
2016-11-12 10:21:12 +0800

    修改新建组样式

--- application/views/admin/user/sidebar.php
+++ application/views/admin/user/sidebar.php
@@ -1,1 +1,1 @@
-<a href="<?php e::url('user/register');?>" class="side-bar btn btn-primary"><?php echo(__('admin.user.sidebar.new')); ?></a>
+<!-- <a href="<?php e::url('user/register');?>" class="side-bar btn btn-primary"><?php echo(__('admin.user.sidebar.new')); ?></a> -->
