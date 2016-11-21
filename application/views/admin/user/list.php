0c8b723bf2531f8bb49a7005ac0035e0a9128c8f
zl <854976866@qq.com>
2016-11-21 13:14:10 +0800

    用户数据表

--- application/views/admin/user/list.php
+++ application/views/admin/user/list.php
@@ -16,7 +16,7 @@
             <tbody>
             <?php foreach($user_list as $u):?>
                 <tr>
-                    <td><?php echo HTML::anchor("/u/{$u['user_id']}", $u['user_id']); ?></td>
+                    <td><?php echo HTML::anchor("/admin/user/form?user={$u['user_id']}", $u['user_id']); ?></td>
                     <td><?php echo HTML::chars($u['stage']); ?></td>
                     <td><?php echo $u['solved']; ?></td>
                     <td><?php echo $u['submit']; ?></td>
